3ff9f93b5b5a632158bfac7f7251c8d1db6fdde8
Michael Lenahan <michael_lenahan@hotmail.com>
2021-11-11 12:51:21 +0000

    Update some fields on current models, remove comment model and add tags and sitesettings model

--- blog/models.py
+++ blog/models.py
@@ -9,18 +9,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
-
+    
     class Meta:
         ordering = ["-created_on"]
 
@@ -31,17 +27,18 @@ class Post(models.Model):
         return self.likes.count()
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+class Tags(models.Model):
     name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return {self.name}
+
+class SiteSettings(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    sub_title = models.CharField(max_length=100, unique=True)
+    social_media_links = models.URLField(max_length=128, db_index=True, unique=True, blank=True)
