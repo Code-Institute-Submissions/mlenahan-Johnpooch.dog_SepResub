e26105c706d7188447b1606a47f8a6fe8e1fe9c8
Michael Lenahan <michael_lenahan@hotmail.com>
2021-12-10 17:31:43 +0000

    add functioning tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Post
+from .models import Post, SiteSettings
 
 
 class PostList(generic.ListView):
@@ -15,11 +15,15 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        site_settings = SiteSettings.objects.get()
+        tags = post.tags.all()
 
         return render(
             request,
             'post_detail.html',
             {
                 'post': post,
+                'site_settings': site_settings,
+                'tags': tags,
             },
         )
