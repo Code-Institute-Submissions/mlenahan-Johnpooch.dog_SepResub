93d5d6253c864f99dddb3ab0b02c15960cf9ad99
Michael Lenahan <michael_lenahan@hotmail.com>
2022-01-13 21:18:12 +0000

    modify django auth templates for login and sign up

--- blog/views.py
+++ blog/views.py
@@ -42,10 +42,7 @@ class PostDetail(View):
 
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        comments = post.comments.filter(approved=True).order_by("-created_at")
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
