ad2db11031cedded141be5b698ba540c5f01bbaf
Michael Lenahan <michael_lenahan@hotmail.com>
2022-06-10 13:15:19 +0000

    add logging for debug

--- django_blog/settings/prod.py
+++ django_blog/settings/prod.py
@@ -1,7 +1,7 @@
-from .base import *
+import os
 import dj_database_url
 import django_heroku
-import os
+from .base import *
 
 
 DATABASES = {
@@ -19,3 +19,29 @@ django_heroku.settings(locals())
 # DEBUG = os.environ['DEBUG_VALUE'] == 'TRUE'
 
 DEBUG = False
+
+import logging.config
+LOGGING_CONFIG = None
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+    # root logger
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    },
+})
