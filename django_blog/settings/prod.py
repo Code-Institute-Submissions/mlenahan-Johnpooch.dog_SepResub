c149f998e71b752a38d10c259383b30de9a53654
Michael Lenahan <michael_lenahan@hotmail.com>
2022-07-02 14:50:50 +0000

    settings fix attampt for prod 5

--- django_blog/settings/prod.py
+++ django_blog/settings/prod.py
@@ -8,7 +8,7 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ['DATABASE_URL'])
 }
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://django-blog-ml.herokuapp.com/']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -16,3 +16,36 @@ django_heroku.settings(locals())
 
 # DEBUG = os.environ.get('DEBUG', False)
 DEBUG = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
