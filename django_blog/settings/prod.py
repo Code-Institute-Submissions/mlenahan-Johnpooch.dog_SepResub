db225c984b63313b7f58a5bad323137511a51a0c
Michael Lenahan <michael_lenahan@hotmail.com>
2022-07-02 14:20:02 +0000

    settings fix attampt for prod

--- django_blog/settings/prod.py
+++ django_blog/settings/prod.py
@@ -14,42 +14,4 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 django_heroku.settings(locals())
 
-# DEBUG = 'DEBUG' in os.environ
-
-# DEBUG = os.environ['DEBUG_VALUE'] == 'TRUE'
-
-DEBUG = False
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
-}
+os.environ['DEBUG_VALUE'] == 'True'
