28a2a963e6c79a97c6fe803e575cb20d9689239d
Michael Lenahan <michael_lenahan@hotmail.com>
2022-07-05 13:58:42 +0000

    fix attempt 2

--- django_blog/settings/settings.py
+++ django_blog/settings/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
 }
 
 # Password validation
