a80b84f95b37db757dc9b3602fe0856aec0a9e29
Michael Lenahan <michael_lenahan@hotmail.com>
2022-07-07 11:12:14 +0000

    add blank true to email field in comment

--- blog/models.py
+++ blog/models.py
@@ -76,7 +76,7 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
         related_name='comments')    
     name = models.CharField(max_length=80)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
